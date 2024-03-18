9cbc5f26e3d9aa8910926822ba8904f760d98b3f
Kcenia Bougrova <kcenia.bougrova@research.fchampalimaud.org>
2024-03-18 16:45:14 +0000

    description of needed files

--- loop_sessions.py
+++ loop_sessions.py
@@ -3,4 +3,22 @@
 KceniaB
 
 
-"""
+""" 
+
+
+""" 
+    1. get data files 
+        df_np
+        df_di
+        df_behav
+        
+    2. add needed columns
+        session day
+        mouse name
+        task protocol
+        region 
+    
+""" 
+
+
+
