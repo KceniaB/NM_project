c0c984f36cddd151f256b0bdfd1d5e5f215e1b36
KceniaB <kcenia.bougrova@research.fchampalimaud.org>
2024-04-17 23:25:42 +0100

    some more code

--- loop_sessions.py
+++ loop_sessions.py
@@ -84,4 +84,90 @@ axs[1].legend(['ph', 'pbod'])
 
 
 
+#%%
+intervals_0_event = np.sort(np.r_[df_trials['intervals_0'].values]) 
+len(intervals_0_event)
+
+# transform the nph TTL times into bpod times 
+df_PhotometryData = df_nph
+nph_sync = fcn_nph_to_bpod_times(tph[iph]) 
+bpod_times = tbpod[ibpod] 
+bpod_sync = bpod_times
+bpod_1event_times = tbpod[ibpod]
+nph_to_bpod = nph_sync
+fig1, ax = plt.subplots()
+
+ax.set_box_aspect(1)
+plt.plot(nph_to_bpod, bpod_1event_times) 
+plt.show()
+
+df_PhotometryData["bpod_frame_times_feedback_times"] = fcn_nph_to_bpod_times(df_PhotometryData["Timestamp"]) 
 # %%
+
+# Assuming nph_sync contains the timestamps in seconds
+nph_sync_start = nph_sync[0] - 60  # Start time, 100 seconds before the first nph_sync value
+nph_sync_end = nph_sync[-1] + 60   # End time, 100 seconds after the last nph_sync value
+
+# Select data within the specified time range
+selected_data = df_PhotometryData[
+    (df_PhotometryData['bpod_frame_times_feedback_times'] >= nph_sync_start) &
+    (df_PhotometryData['bpod_frame_times_feedback_times'] <= nph_sync_end)
+]
+
+# Now, selected_data contains the rows of df_PhotometryData within the desired time range 
+selected_data 
+
+# Plotting the new filtered data 
+plt.figure(figsize=(20, 10))
+plt.plot(selected_data.bpod_frame_times_feedback_times, selected_data[region],color = "#25a18e") 
+
+xcoords = nph_sync
+for xc in zip(xcoords):
+    plt.axvline(x=xc, color='blue',linewidth=0.3)
+plt.title("Entire signal, raw data")
+plt.legend(["GCaMP","isosbestic"],frameon=False)
+sns.despine(left = False, bottom = False) 
+# plt.axvline(x=init_idx) 
+# plt.axvline(x=end_idx) 
+plt.show()
+# %%
+df_PhotometryData = selected_data
+
+
+df_PhotometryData = df_PhotometryData_1.reset_index(drop=True)  
+df_470 = df_PhotometryData[df_PhotometryData.LedState==2] 
+df_470 = df_470.reset_index(drop=True)
+df_415 = df_PhotometryData[df_PhotometryData.LedState==1] 
+df_415 = df_415.reset_index(drop=True) 
+#================================================
+""" 4.1.4 FRAME RATE """ 
+acq_FR = find_FR(df_470["Timestamp"]) 
+
+# %%
+raw_reference = df_415[region] #isosbestic 
+raw_signal = df_470[region] #GCaMP signal 
+raw_timestamps_bpod = df_470["bpod_frame_times_feedback_times"]
+raw_timestamps_nph_470 = df_470["Timestamp"]
+raw_timestamps_nph_415 = df_415["Timestamp"]
+raw_TTL_bpod = bpod_sync
+raw_TTL_nph = nph_sync
+
+plt.plot(raw_signal[:],color="#60d394")
+plt.plot(raw_reference[:],color="#c174f2") 
+plt.legend(["signal","isosbestic"],fontsize=15, loc="best")
+plt.show() 
+# %%
+
+my_array = np.c_[raw_timestamps_bpod, raw_reference, raw_signal]
+
+df = pd.DataFrame(my_array, columns=['times', 'raw_isosbestic', 'raw_calcium'])
+
+df_photometry = iblphotometry.dsp.baseline_correction_dataframe(df)
+
+
+fig, ax = iblphotometry.plots.plot_raw_data_df(df_photometry)
+
+
+
+
+
